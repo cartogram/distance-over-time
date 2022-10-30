9228fc06b27a37b1c821dbbd1669d3dc2612debc
Matt Seccafien <mseccafien@gmail.com>
2022-10-30 23:03:38 +0100

    Update Text.tsx

--- packages/ui/src/Text.tsx
+++ packages/ui/src/Text.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export function Text({children}: React.PropsWithChildren) {
-  return <p style={{textDecoration: '', color: 'blue'}}>{children}</p>
+  return <p style={{textDecoration: 'underline'}}>{children}</p>
 }

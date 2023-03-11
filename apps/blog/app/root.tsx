281df3404afc9812eb7105a095c5d98cf2edf376
Matt Seccafien <mseccafien@gmail.com>
2023-03-11 03:28:21 +0100

    trying to deploy

--- apps/blog/app/root.tsx
+++ apps/blog/app/root.tsx
@@ -15,13 +15,7 @@ export const links: LinksFunction = () => [
   {rel: 'stylesheet', href: styles},
   {
     rel: 'stylesheet',
-    href:
-      // 'https://api.fontshare.com/v2/css?f[]=stardom@400&f[]=jet-brains-mono@1,2&f[]=general-sans@400&display=swap',
-      'https://api.fontshare.com/v2/css?f[]=jet-brains-mono@1,2&f[]=general-sans@400&f[]=paquito@1&f[]=stardom@400&display=swap',
-
-    // 'https://api.fontshare.com/v2/css?f[]=stardom@400&f[]=paquito@400&f[]=tabular@2,1&f[]=jet-brains-mono@1,2&display=swap',
-    // 'https://api.fontshare.com/v2/css?f[]=stardom@400&f[]=satoshi@1&display=swap',
-    // 'https://api.fontshare.com/v2/css?f[]=switzer@1&f[]=erode@2,1&display=swap',
+    href: 'https://api.fontshare.com/v2/css?f[]=jet-brains-mono@1,2&f[]=general-sans@400&f[]=paquito@1&f[]=stardom@400&display=swap',
   },
 ]
 
